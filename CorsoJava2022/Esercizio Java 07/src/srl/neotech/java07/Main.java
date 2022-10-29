2046fdfc92a149998da3de9d9d3a3f4ded5ec0bc
fGironi <f.gironi@neotech.srl>
2022-10-29 10:38:39 +0200

    Cleanup Java07

--- CorsoJava2022/Esercizio Java 07/src/srl/neotech/java07/Main.java
+++ CorsoJava2022/Esercizio Java 07/src/srl/neotech/java07/Main.java
@@ -16,6 +16,5 @@ public class Main {
 			else if (g instanceof Mostro) {	((Mostro) g).caccia(); ((Mostro) g).vaInLetargo();}
 			System.out.println();
 		}
-		System.out.println("tutto su github");
 	}
 }
