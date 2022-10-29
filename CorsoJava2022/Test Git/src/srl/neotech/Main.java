76195c4983a1e2b5fb8fc0f98b27a4b6cce7019c
fGironi <f.gironi@neotech.srl>
2022-10-29 10:58:14 +0200

    Preparazione al merge

--- CorsoJava2022/Test Git/src/srl/neotech/Main.java
+++ CorsoJava2022/Test Git/src/srl/neotech/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-
+		System.out.println("tutto su github");
+		System.out.println("cambiocolore");
+		System.out.println("panico da merge!");
+		Integer panico=1000;
 	}
 
 }
