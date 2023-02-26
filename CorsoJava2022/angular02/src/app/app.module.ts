7d2fb833bbc24df9fa5459cf1170e4e63bb4d420
FGironi <f.gironi@neotech.srl>
2023-02-26 14:21:14 +0100

    various bindings

--- CorsoJava2022/angular02/src/app/app.module.ts
+++ CorsoJava2022/angular02/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
@@ -24,6 +25,8 @@ import { CardComponent } from './card/card.component';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CorsoJava2022/angular02/src/app/card/card.component.ts
+++ CorsoJava2022/angular02/src/app/card/card.component.ts
@@ -14,30 +14,43 @@ export class CardComponent implements OnInit {
 
   counter:number=0;
 
+  testo:string="prova"
+
   cani=[
-    { nome: 'Doggo',
-      razza: 'Razza di cane',
+    { nome: 'Doggo0',
+      razza: 'Razza di cane1',
       descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
                      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-                     bred for hunting.`
+                     bred for hunting.`,
+      isLiked: false
     },
-    { nome: 'Doggo2',
+    { nome: 'Doggo1',
       razza: 'Razza di cane2',
       descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
                      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-                     bred for hunting.`
+                     bred for hunting.`,
+      isLiked: false
     },
-    { nome: 'Doggo3',
+    { nome: 'Doggo2',
       razza: 'Razza di cane3',
       descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
                      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-                     bred for hunting.`
-    }
-
-
+                     bred for hunting.`,
+      isLiked: false
+                    }
 
   ]
 
+  increaseCounter():void{
+    this.counter++
+    if (this.counter>=this.cani.length) this.counter=0;
+  }
 
+  likeDoggo(event: Event){
+    this.cani[this.counter].nome="Doggo"+this.counter+" <3";
+    this.cani[this.counter].isLiked=true;
+
+
+  }
 
 }
